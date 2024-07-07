12354712caa0748e4de64cc06eedcfd406bf1737
Natsugure <natsugure5301102@gmail.com>
2024-07-07 10:29:02 +0900

    Initial Commit

---
+++ noteAnalyzer/noteAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  noteAnalyzerApp.swift
+//  noteAnalyzer
+//
+//  Created by 秋空 on 2024/07/07.
+//
+
+import SwiftUI
+
+@main
+struct noteAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
